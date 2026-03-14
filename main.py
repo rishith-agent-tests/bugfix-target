4bbe549354a011130de805cf9c592142c964c3b2
Rishith Minupala <rishithminupala@rishithminupala.local>
2026-03-14 16:51:33 +0530

    test-1

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from src.calc import add
+
+if __name__ == "__main__":
+    print("add(2, 3) =", add(2, 3))
+    print("add(2, -1) =", add(2, -1))
+    print("add(-1, 5) =", add(-1, 5))
---
+++ src/calc.py
@@ -0,0 +1,4 @@
+def add(a: int, b: int) -> int:
+    if b < 0:
+        return a - b
+    return a + b
